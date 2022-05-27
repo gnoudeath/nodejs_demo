94811b0b5e7fad1801149b4dfe44cad058f48328
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2022-05-27 20:01:09 +0700

    cm-v2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const hbs = require('express-handlebars');
+const { extname } = require('path');
 const app = express();
 const port = 3000;
 
@@ -9,12 +10,18 @@ const port = 3000;
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('handlebars', hbs.engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', hbs.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources','views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  res.render('news');
 })
 
 
