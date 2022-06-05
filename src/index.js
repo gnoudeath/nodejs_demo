d86b7c204357bbe60f53fb36a9c149b83d77dbf3
gnoudeath <87683653+gnoudeath@users.noreply.github.com>
2022-06-05 16:13:47 +0700

    commit v4

--- src/index.js
+++ src/index.js
@@ -6,10 +6,17 @@ const { extname } = require('path');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// XMLHttpRequest, fetch, axios
+
 // HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', hbs.engine({
@@ -18,15 +25,9 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname,'resources','views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/news', (req, res) => {
-  res.render('news');
-})
-
+//routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
